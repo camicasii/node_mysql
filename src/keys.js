aba2e5518c5bef2747dc8517588d54f01da06e80
camicasii <camicasii@hotmail.com>
2019-09-04 21:28:48 -0400

    configuracion de database terminada key agregadas

---
+++ src/keys.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+    database:{
+        host:"localhost",
+        user:"camicasii",
+        password:"159753",
+        database:"database_links"
+    },
+    cleverCloud:{
+        DB:"b2fcev1eevxoxvzog5bi",
+        HOST:"b2fcev1eevxoxvzog5bi-mysql.services.clever-cloud.com",
+        PASSWORD:"BFJmOuJN7m06h1d705fc",
+        PORT:"3306",
+        URI:"mysql://upakyglkp4jtubxy:BFJmOuJN7m06h1d705fc@b2fcev1eevxoxvzog5bi-mysql.services.clever-cloud.com:3306/b2fcev1eevxoxvzog5bi",
+        USER:"upakyglkp4jtubxy",
+        VERSION:"8.0"
+    }
+
+}
