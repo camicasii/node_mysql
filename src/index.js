80c3d1b42dfa5bbbfd6fa62e0f49cadcdde0c928
camicasii <camicasii@hotmail.com>
2019-09-04 20:21:06 -0400

    configuracion basica del servidor

--- src/index.js
+++ src/index.js
@@ -32,10 +32,13 @@ app.use((req,res,next)=>{
 })
 
 //Routes
-app.use(require('./routes/'))
+app.use(require('./routes/'));
+app.use(require('./routes/authentication'));
+app.use('/links', require('./routes/links'));
 
 //Public
 
+app.use(express.static(path.join(__dirname,'public')))
 
 //Starting of server
 
