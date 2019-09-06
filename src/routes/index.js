7a385fb78fcbf04ba982861014749e5c2990d716
camicasii <camicasii@hotmail.com>
2019-09-05 21:48:21 -0400

    se agrego el codigo para hacer un crud basico con mysql ver archivo link

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
-
+const pool = require('../database')
 const router = express.Router();
 
 router.get('/',(req,res)=>{
     res.send("Hola Mundo")
 })
 
+router.get('/signup', async(req,res)=>{       
+
+    await pool.query('ALTER TABLE links AUTO_INCREMENT = 1')//reinicial id de la base de datos a 1
+    console.log("paso");
+    
+    res.redirect("/links/")
+    
+})
 module.exports = router;
